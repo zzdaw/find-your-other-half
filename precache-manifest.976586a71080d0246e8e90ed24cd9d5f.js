c1d8feb500804483bf638ec9ffb44a72f307e2d8
zzdaw <hohi@wp.pl>
2019-03-05 21:23:46 +0100

    Updates

---
+++ precache-manifest.976586a71080d0246e8e90ed24cd9d5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8fc1994431a9d7571b75cb03e6debbf",
+    "url": "/find-your-other-half/static/media/img2.f8fc1994.jpg"
+  },
+  {
+    "revision": "620832ea5d97266709bb",
+    "url": "/find-your-other-half/static/css/main.b1492abb.chunk.css"
+  },
+  {
+    "revision": "83f33013b7e4a8863e6c",
+    "url": "/find-your-other-half/static/js/runtime~main.83f33013.js"
+  },
+  {
+    "revision": "74a558fb32757b973c7e",
+    "url": "/find-your-other-half/static/js/2.74a558fb.chunk.js"
+  },
+  {
+    "revision": "9051483463a188d565d9c869c4fe27dc",
+    "url": "/find-your-other-half/static/media/img1.90514834.jpg"
+  },
+  {
+    "revision": "620832ea5d97266709bb",
+    "url": "/find-your-other-half/static/js/main.620832ea.chunk.js"
+  },
+  {
+    "revision": "d88493c70332e47a40a3e0a2eb3cd850",
+    "url": "/find-your-other-half/static/media/img3.d88493c7.jpg"
+  },
+  {
+    "revision": "bc7950e1696f21ae012901e7c54a9dbd",
+    "url": "/find-your-other-half/static/media/img4.bc7950e1.jpg"
+  },
+  {
+    "revision": "70e29f67583912dbcaf3aa3556afe2cf",
+    "url": "/find-your-other-half/static/media/img5.70e29f67.jpg"
+  },
+  {
+    "revision": "2757b16398b018065c6c7bca7f67bec6",
+    "url": "/find-your-other-half/static/media/transparent.2757b163.png"
+  },
+  {
+    "revision": "ad2f0138e6d03908d76899a4c9a0f9cd",
+    "url": "/find-your-other-half/index.html"
+  }
+];
