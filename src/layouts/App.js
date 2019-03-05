582e54d5f41d0c4572b2a5a48a371765a72fde00
zzdaw <hohi@wp.pl>
2019-03-05 22:49:33 +0100

    Practice

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   // }
   render() {
     return (
-      <Router basename='/'>
+      <Router basename='/male'>
         <div className='app'>
           <header>
             {<Header />}
